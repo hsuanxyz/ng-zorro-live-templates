73c70cc9fc11ecb2eee33b88c20c6b698d55b711
Hsuan Lee <HsuanGM@gmail.com>
2018-01-10 11:16:43 +0800

    feat: add replaceTemplate & replaceDescription methods

--- build.js
+++ build.js
@@ -11,7 +11,13 @@ function _read(file) {
     return fs.readFileSync(file, 'utf8');
 }
 
-function _replaceSymbol(template) {
+/**
+ * 替换模板特殊字符
+ * @param template
+ * @returns {string}
+ * @private
+ */
+function _replaceTemplate(template) {
     template = template.replace(/\n/g, '&#10;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
@@ -20,8 +26,41 @@ function _replaceSymbol(template) {
     return JSON.stringify(template);
 }
 
+/**
+ * 替换默认值特殊字符
+ * @param defaultValue
+ * @returns {*}
+ * @private
+ */
+function _replaceDefaultValue(defaultValue) {
+    if (defaultValue) {
+        return defaultValue.replace(/"/g, '&quot;')
+    } else {
+        return '';
+    }
+}
+
+/**
+ * 替换描述特殊字符
+ * @param description
+ * @returns {string}
+ * @private
+ */
+function _replaceDescription(description) {
+    // 将模板变量替换成 ...
+    // 并压缩成一行
+    return description.replace(/\$.+?\$/g, '...')
+    .replace(/\n/g, '')
+    .replace(/\s\s/g, ' ')
+}
+
+/**
+ * 读取 src/templates 下全部的 yaml 文件，合并返回
+ * @returns {string}
+ */
 function readTemplates() {
     let template = '';
+    // TODO 只读 .yaml
     const templateFiles = fs.readdirSync('./src/templates');
     templateFiles.forEach(file => {
         template += _read('./src/templates/' + file)
@@ -29,15 +68,24 @@ function readTemplates() {
     return template;
 }
 
+
 function yaml2xml() {
     const data = [];
     Object.keys(templates).forEach(k => {
         const t = templates[k];
         let tpl = t.tpl;
+        
+        // 处理 defaultValue 中的特殊字符
+        if (Array.isArray(t.variables)) {
+            t.variables = t.variables.map(val => {
+                val.defaultValue = _replaceDefaultValue(val.defaultValue);
+                return val;
+            })
+        }
         const snippet = {
             name: k,
-            description: `nz: ${t.description || t.tpl}`,
-            tpl: _replaceSymbol(tpl),
+            description: `nz: ${t.description || _replaceDescription(t.tpl)}`,
+            tpl: _replaceTemplate(tpl),
             variables: t.variables || [],
             tplRaw: tpl
         };
